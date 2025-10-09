b1172b619317e47ae384020a1074c556c4d338e8
romangolik877-wq <romangolik877@gmail.com>
2025-10-09 15:13:47 +0500

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp5
+{
+    class Сar
+    {
+        public Сar()
+        {
+
+        }
+        public Сar(int x)
+        {
+            Console.WriteLine("from Without Pra Cost");
+        }
+        public Сar(double y)
+        {
+            
+        }
+        private int Id;
+        internal string Name;
+        protected  DateTime CreateDate;
+        protected internal double Price;
+ 
+        
+    }
+    
+}
+    
+
+
+
+
+
+
+
+
